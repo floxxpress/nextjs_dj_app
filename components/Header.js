97a3770b64453f56f8e67f0fc399aa9b9d99c3a9
floxxpress <floxpress@gmail.com>
2021-05-31 21:24:29 +0100

    Add Authorisation page to project

--- components/Header.js
+++ components/Header.js
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import {FaSignInAlt, FaSignOutAlt} from "react-icons/fa"
 import styles from '@/styles/Header.module.css'
 import Search from "@/components/Search";
 function Header(props) {
@@ -14,6 +15,9 @@ function Header(props) {
                 <ul>
                     <li><Link href='/events'><a>Events</a></Link></li>
                     <li><Link href='/events/add'><a>Add Event</a></Link></li>
+                    <li><Link href='/account/login'><a className={'btn-secondary btn-icon'}>
+                       <FaSignInAlt /> Login</a>
+                    </Link></li>
                 </ul>
             </nav>
 
